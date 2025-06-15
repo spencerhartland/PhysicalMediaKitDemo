6498c6f139666b642d6ad4ab316275e6cfae3375
Spencer Hartland <spencerhartland@icloud.com>
2025-06-15 12:54:42 -0700

    Initial Commit

---
+++ PhysicalMediaKitDemo/PhysicalMediaKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicalMediaKitDemoApp.swift
+//  PhysicalMediaKitDemo
+//
+//  Created by Spencer Hartland on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PhysicalMediaKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
